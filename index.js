c4681fb2543ea8ce0c28434aa65e73e57b177588
Iacopo Santi <iacopo.santi@studenti.isissgobetti.it>
2023-04-19 08:46:56 +0200

    formattato

--- index.js
+++ index.js
@@ -1,12 +1,5 @@
 const prompt = require("prompt-sync")();
 
-class Auto {
-    constructor(targa,carburante,matricola,potenza) {
-        this.targa = targa;
-        this.carburante = carburante;
-        this.matricola = matricola;
-        this.potenza = potenza;
-    }
+const input = prompt("Scrivi il tuo nome:");
 
-    
-}
+console.log(`Il tuo nome è ${input}`);
