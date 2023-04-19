654e37b8a9f024f1a000395726c7c621dd8598e9
Iacopo Santi <iacopo.santi@studenti.isissgobetti.it>
2023-04-19 08:15:30 +0200

    pull

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const prompt = require("prompt-sync")();
 
-const input1 = prompt("What's your name?");
-console.log(`Your name is ${input1}`);
+class Auto {
+    constructor(targa,carburante,matricola,potenza) {
+        this.targa = targa;
+        this.carburante = carburante;
+        this.matricola = matricola;
+        this.potenza = potenza;
+    }
 
-const input2 = prompt("What's your surname?");
-console.log(`Your surname is ${input2}`)
-
-console.log(`Your full name is ${input1} ${input2}`);
+    
+}
