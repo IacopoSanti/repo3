5515f045337d36ca106f18bdbf8c04238f060e96
Iacopo Santi <iacopo.santi@studenti.isissgobetti.it>
2023-05-10 08:54:33 +0200

    modified:   importa_json.js

--- importa_json.js
+++ importa_json.js
@@ -1,3 +1,3 @@
-var jsonString = JSON.stringify(/home/santi/code/repo3-1/json_da_importare.json);
-console.log(jsonString); 
+var jsonStringa = JSON.stringify(/home/santi/code/repo3-1/json_da_importare.json);
+console.log(jsonStringa); 
   
