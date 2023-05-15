77923c9e8b74478d9bdc1ad5f45944e649df32af
Iacopo Santi <iacopo.santi@studenti.isissgobetti.it>
2023-05-15 13:40:07 +0200

    modified:   Santi_Iacopo_4M_JSON.js

--- Santi_Iacopo_4M_JSON.js
+++ Santi_Iacopo_4M_JSON.js
@@ -4,8 +4,10 @@ class Persona {
         this.cognome = cognome;
         this.data_di_nascita = data_di_nascita;
     }
-
-    
+        parsePersona () {
+            const personaJson = JSON.parse(Persona.json);
+            console.log(personaJson);
+        }
 }
 
 class Istituzione {
@@ -13,4 +15,10 @@ class Istituzione {
         this.nome_istituzione = nome_istituzione;
         this.lista_personale = lista_personale;
     }
+
+    parseIstituzione () {
+        const istituzioneJson = JSON.parse(Istituzione.json);
+        console.log(istituzioneJson);
+    }
+
 }
